9fb7349c5690e12bbfaa2453a0e32f7d7d69eac6
Gaby <gmartellv@unsa.edu.pe>
2023-05-05 11:38:19 -0500

    segundo commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,9 @@ public class HolaMundo
     public static void main(String args[]) {
  // primer commit
         System.out.println("Hola Mundo");
+
+// segundo commit
+
+        System.out.println("Bienvenido al curso");
     }
 }
